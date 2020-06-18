2ab2cb1e112b3fa2e1549a4afbf56b26adb7e3ed
Affinitybusinesssuite <61192738+Affinitybusinesssuite@users.noreply.github.com>
2020-06-18 16:12:40 +0500

    Update preprintbill.js

--- ginsoy-Production/ginsoy-Production/pos_kot_bill/static/src/js/preprintbill.js
+++ ginsoy-Production/ginsoy-Production/pos_kot_bill/static/src/js/preprintbill.js
@@ -93,14 +93,14 @@ var PreBillScreenWidget = screens.ReceiptScreenWidget.extend({
 	                orderlines: order.get_orderlines(),
 	                paymentlines: order.get_paymentlines(),
 	            };
-	            var receipt_html = QWeb.render('PosTicket',env);
+	           /* var receipt_html = QWeb.render('PosTicket',env);
 	        	order.set_pos_normal_receipt_html(receipt_html.replace(/<img[^>]*>/g,"").replace(/<object[^>]*>/g,"")); 
-	        /*	var receipt = QWeb.render('XmlReceipt',env);
+	        	var receipt = QWeb.render('XmlReceipt',env);
 	        	order.set_pos_xml_receipt_html(receipt.replace(/<img[^>]*>/g,"").replace(/<object[^>]*>/g,"")); */
 			console.log("ok yhn tk: ");				
 	        	self.pos.push_order(order);
 			console.log("Picking ID ",order.picking_id);
-            	self.pos.gui.show_screen('receipt'); 
+            	self.pos.gui.show_screen('products'); 
 			}
 	    console.log("Picking ID1",order.picking_id);
 	     console.log("END");
